9ef13bb5cb46a9e707fdc845123114d525dbcb03
Saurabh Soni <ace@Star>
2025-07-31 15:51:26 +0530

    hello kernel

--- boot.asm
+++ boot.asm
@@ -1,5 +1,7 @@
-bits	16				; 16 bit real mode
-org		0x7C00			; legacy boot adr
+; boot.asm
+
+bits		16						; 16 bit real mode
+org		0x7C00					; legacy boot adr
 
 start:
 	cli
@@ -9,9 +11,27 @@ start:
 	mov		ss, ax
 	mov		sp, ax
 
+	mov		si, msg
+	call		print_fn
+
+end:
 	cli
 	hlt
 
+print_fn:
+	lodsb						; load byte form [si] into al
+	or		al, al
+	jz		.done
+	mov		ah, 0x0E
+	int		0x10
+	jmp		print_fn
+.done:
+	ret
+
+; data
+msg		db "Hello, Kernel!", 0x0D, 0x0A, 0x0
+
 ; boot signature
-times 	510-($-$$) db 0
-dw 		0xAA55			; magic number
+times 	510-($-$$) db 0x0
+dw 		0xAA55					; magic number
+
